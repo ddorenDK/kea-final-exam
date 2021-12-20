8e30076551c3854d35ff2a77f8f93c1d56155bea
Dorin M <89907196+ddorenCorti@users.noreply.github.com>
2021-12-20 22:30:02 +0100

    added debug mode for get_epd_url method

--- site_to_links.py
+++ site_to_links.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-#Creates a list with download links for each epd available on epddanmark site
-def get_epd_urls(base_url, database_url):
+
+def get_epd_urls(base_url, database_url, debug = False):
+    # arg 1 = base_url of the site used in the creation of the output links
+    # arg 2 = url with the location of the pdfs
     url_list = []
     page = requests.get(database_url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -14,6 +16,15 @@ def get_epd_urls(base_url, database_url):
             if not href.startswith('/media'):
                 continue
             url_list.append(base_url + href)
+    #Debug option
+    if debug is True:
+        print ("############### GET # EPD # URLS ##############")
+        # Debug Mode On; get_epd_urls; Printing The Urls
+        print (f'> Urls found: {len(url_list)}')
+        print ("> Printing the URL List:")
+        print (url_list)
+        print ("###############################################")
     return url_list
+
 
 
