cedadab8adad8d5f2e39f36e2ba37c3e5abdebf8
Dorin M <89907196+ddorenDK@users.noreply.github.com>
2021-12-30 03:43:51 +0200

    added some changes to exec the code

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ temp_pdf_location = "./temp_pdf/single_epd.pdf"
 
 
 ##################################################################   SITE TO LINKS   #################################################################################
-# 1
+#1.1
 
-def get_epd_urls(base_url, database_url, debug = False, limit = -1):
+def get_epd_urls(base_url, database_url, debug = False, limit = 30):
     # arg 1 = base_url of the site used in the creation of the output links
     # arg 2 = url with the location of the pdfs
     it = 1
@@ -54,7 +54,7 @@ def get_epd_urls(base_url, database_url, debug = False, limit = -1):
 ########################################################################################################################################################################
 ##################################################################   LINKS TO PDFS   ###################################################################################
 
-#2.1
+#1.2
 # Downloads the pdf from a single URL
 def link_to_pdf(url, debug = False):
 
@@ -64,12 +64,8 @@ def link_to_pdf(url, debug = False):
             print (f'>Method: link_to_pdf \nFound no temp_pdf directory \n>Creating folder at {temp_directory_location} \n')
         # -----
         os.makedirs(temp_directory_location)
-    else:
-        # DEBUG
-        if debug:
-            print (f'>Method: link_to_pdf \n>Found existing temp_pdf directory \n')
-        # -----
-            
+    elif debug:
+        print('>Method: link_to_pdf \n>Found existing temp_pdf directory \n')
     r = requests.get(url, allow_redirects=True)
     # DEBUG
     if debug:
@@ -88,12 +84,8 @@ def links_to_pdfs(urls, debug = False, temp_directory_location = "./temp_pdf"):
             print (f'>Method: links_to_pdfs \nFound no temp_pdf directory \n>Creating folder at {temp_directory_location} \n')
         # -----
         os.makedirs(temp_directory_location)
-    else:
-        # DEBUG
-        if debug:
-            print (f'>Method: links_to_pdfs \n>Found existing temp_pdf directory \n')
-        # -----
-
+    elif debug:
+        print('>Method: links_to_pdfs \n>Found existing temp_pdf directory \n')
     for url in urls:
         # This will be the location of the url, the full download url is split by / and the last part is saved as the name of the pdf file using s.split('_')[-1]
         full_temp_pdf_location = temp_directory_location + '/' + url.split('/')[-1]
@@ -109,42 +101,48 @@ def links_to_pdfs(urls, debug = False, temp_directory_location = "./temp_pdf"):
 
 
 ########################################################################################################################################################################
-##################################################################   FILTER   ##########################################################################################
+##################################################################   PDFS TO PYTHON OBJECT  ############################################################################
 
-#3.1
+#2.1
 # Gets a pdf and returns a STRING with the rows containing GWP or GWP-total
-def extract_gwp_from_pdf(pdf_location, debug=False):
-    found_rows = ''
-    # DEBUG
-    if debug:
-        print(f'>Starting extraction from the pdf at {pdf_location}')
-    # -----
-    pdf = pdfplumber.open(pdf_location)
-    # DEBUG
-    if debug:
-        print(f'>Pages in the epd pdf file: {len(pdf.pages)}')
-        # Variable used for iteration
-        it = 1
-    # -----
-    for page in pdf.pages:
+def extract_gwp_from_pdf(pdfs_location='./temp_pdf', debug=False):
+    #list that will hold found gwps in a string format
+    found_gwps = []
+    for file in os.listdir(pdfs_location):
+        #String that will contain information about one extracted epd from a pdf
+        found_gwp = ''
+        pdf_location = pdfs_location + '/' + file
         # DEBUG
         if debug:
-            print(f'>Started checking for tables in page nr {it}/{len(pdf.pages)}')
-            it += 1
+            print(f'>>>Starting extraction from the pdf at {pdf_location}<<<')
         # -----
-        #rows will be a list of lists
-        rows = page.extract_table()
-        if not rows is None:
-            for row in rows:
-                #now working with a sinlge list
-                if 'GWP-total' in row or 'GWP' in row:
-                    if debug: 
-                        print(f'Found a row containing GWP or GWP total on a table on the page {it}/{len(pdf.pages)}')
-                        print(f'Row containing \'GWP\' or \'GWP-total\': {row}')
-                    rowToSave = list_to_string(row)
-                    found_rows = found_rows + rowToSave + ' | '
-                    print(rows[0])
-    return found_rows
+        pdf = pdfplumber.open(pdf_location)
+        # DEBUG
+        if debug:
+            print(f'>Pages in the epd pdf file: {len(pdf.pages)}')
+            # Variable used for iteration
+            it = 1
+        # -----
+        for page in pdf.pages:
+            # DEBUG
+            if debug:
+                print(f'>Started checking for tables in page nr {it}/{len(pdf.pages)}')
+                it += 1
+            # -----
+            #rows will be a list of lists
+            rows = page.extract_table()
+            if rows is not None:
+                for row in rows:
+                    #now working with a sinlge list
+                    if 'GWP-total' in row or 'GWP' in row:
+                        if debug: 
+                            print(f'Found a row containing GWP or GWP total on a table on the page {it}/{len(pdf.pages)}')
+                            print(f'Row containing \'GWP\' or \'GWP-total\': {row}')
+                        rowToSave = list_to_string(row)
+                        found_gwp = found_gwp + rowToSave + ' | ' + file + ' /'
+                        # print(rows[0])
+                        found_gwps.append(found_gwp)
+    return found_gwps
 
 
 ########################################################################################################################################################################
@@ -164,13 +162,13 @@ def flush_temp_pdf():
 def flush_all():
     try:
         flush_temp_json()
-    except (FileNotFoundError) as e: 
-        print(f'>WARNING \n>Could not flush temp_json, directory not found')
+    except FileNotFoundError as e:
+        print('>WARNING \n>Could not flush temp_json, directory not found')
         print(f'Error : {e}')
     try: 
         flush_temp_pdf()
-    except (FileNotFoundError) as e: 
-        print(f'>WARNING \n>Could not flush temp_pdf, directory not found')
+    except FileNotFoundError as e:
+        print('>WARNING \n>Could not flush temp_pdf, directory not found')
         print(f'Error : {e}')
 
 ########################################################################################################################################################################
@@ -203,24 +201,19 @@ def move_table_to_saved(table, debug = False):
 # # # Get a list with urls to pdfs
 pdf_links_list = get_epd_urls(base_url, database_url, limit = 10)
 
-# # # # 2.1
-# # # # # Download the pdf from the urls
-# link_to_pdf(pdf_links_list[0], debug = True)
-
-# # # 1.2
-# # # Download all pdfs from list of urls
+# 1.2
 links_to_pdfs(pdf_links_list, debug = True)
 
-# Extract the tables from a pdf
-# pdf_to_tables("./temp_pdf/single_epd.pdf")
 
-# extracted_tables = get_list_json_tables(debug=True)
+# 2
+extracted_gwps = extract_gwp_from_pdf(debug=True)
 
-# exact_table = find_exact_table(extracted_tables)
 
-# move_table_to_saved(exact_table, debug=True)
 
-# flush_all()
+
+
+
+
 
 
 
