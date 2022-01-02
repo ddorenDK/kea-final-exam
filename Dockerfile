98419fc8dd19e022d9f3ca1b04879db6de12d3e0
Dorin M <89907196+ddorenDK@users.noreply.github.com>
2022-01-02 06:29:52 +0200

    added arugment parser

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,5 @@ RUN python3 -m ensurepip
 RUN pip3 install --no-cache --upgrade pip setuptools
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
-RUN apt-get -y install ghostscript
 COPY . .
  
--- main.py
+++ main.py
@@ -5,19 +5,24 @@ import argparse
 
 
 def main():
+
+    # Argument Parser
+    parser = argparse.ArgumentParser(description = "Extracts epd information from edpdanmark and outputs a json file with the data")
+    parser.add_argument("--debug", default=False, help = "Debug Mode; True/False") 
+    args = parser.parse_args()
     
     database_url = 'https://www.epddanmark.dk/epd-databasen/'
     base_url = 'https://www.epddanmark.dk'
 
     # 1.1
     # # # Get a list with urls to pdfs
-    pdf_links_list = get_epd_urls(base_url, database_url, limit = 10)
+    pdf_links_list = get_epd_urls(base_url, database_url, limit = 10, debug = args.debug)
 
     # 1.2
-    links_to_pdfs(pdf_links_list, debug = True)
+    links_to_pdfs(pdf_links_list, debug = args.debug)
 
     # 2
-    extracted_gwps = extract_gwp_from_pdf(debug=True)
+    extracted_gwps = extract_gwp_from_pdf(debug = args.debug)
 
     
 if __name__ == '__main__':
